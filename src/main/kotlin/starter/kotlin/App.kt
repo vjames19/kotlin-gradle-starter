248d9f3ae657ba904d0675cf4284cb767dff18f8
Victor J. Reventos <victor.reventos1@gmail.com>
2017-06-27 14:12:19 -0500

    Kotlin gradle starter with Spek testing.

---
+++ src/main/kotlin/starter/kotlin/App.kt
@@ -0,0 +1,15 @@
+package starter.kotlin
+
+import org.jooby.Jooby
+import org.jooby.Kooby
+
+class App : Kooby({
+    get {
+        val name = param("name").value("Kotlin")
+        "Hello $name!"
+    }
+})
+
+fun main(args: Array<String>) {
+    Jooby.run(::App, args)
+}
